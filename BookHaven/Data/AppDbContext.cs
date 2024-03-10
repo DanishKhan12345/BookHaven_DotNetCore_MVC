bfdf25ad224485ad88b8a03ed0c435b196ee9402
Danish Khan <danishayaan@gmail.com>
2024-03-10 17:56:38 +0530

    Added Category Table,Controller and View

---
+++ BookHaven/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using BookHaven.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookHaven.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options) //Using options to get connection string and pass it to base class of DbContext
+        { 
+       
+        
+        
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
