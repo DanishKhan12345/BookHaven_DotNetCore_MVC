c235b4d212242b8bf1b06dd8222d7db4bea1259b
Danish Khan <danishayaan@gmail.com>
2024-03-10 17:54:14 +0530

    Initial Commit

--- BookHaven/Program.cs
+++ BookHaven/Program.cs
@@ -1,7 +1,13 @@
+using BookHaven.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+// Add EntityFrameworkCore Service
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDbContext>(options=> options.UseSqlServer(builder.Configuration.GetConnectionString("DatabaseConnection")));
+
 
 var app = builder.Build();
 
