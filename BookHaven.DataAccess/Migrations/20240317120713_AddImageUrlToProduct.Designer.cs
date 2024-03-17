f18a797ae42f6224b1db4c10549a4af3b442e18e
Danish Khan <danishayaan@gmail.com>
2024-03-17 18:24:42 +0530

    1.Added Product table , migrations and interfaces 2. Added Product Views

--- BookHaven.DataAccess/Migrations/20240317120713_AddImageUrlToProduct.Designer.cs
+++ BookHaven.DataAccess/Migrations/20240317120713_AddImageUrlToProduct.Designer.cs
@@ -3,6 +3,7 @@ using BookHaven.DataAccess.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BookHaven.DataAccess.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240317120713_AddImageUrlToProduct")]
+    partial class AddImageUrlToProduct
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ BookHaven.DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,25 @@
+﻿using BookHaven.DataAccess.Data;
+using BookHaven.DataAccess.Repository.IRepository;
+using BookHaven.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookHaven.DataAccess.Repository
+{
+    public class ProductRepository : Repository<Product>,IProductRepository
+    {
+        private readonly AppDbContext _appDbContext;
+        public ProductRepository(AppDbContext appDbContext) : base(appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+        public void update(Product product)
+        {
+            _appDbContext.Products.Update(product);
+        }
+    }
+}
