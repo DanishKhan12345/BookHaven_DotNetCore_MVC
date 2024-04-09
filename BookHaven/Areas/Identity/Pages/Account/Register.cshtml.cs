add89ffdea7ad349076616fcaf8dc3eeaf17e671
Danish Khan <danishayaan@gmail.com>
2024-04-09 15:34:32 +0530

    Updated Login and Register screen

--- BookHaven/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ BookHaven/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -106,6 +106,14 @@ namespace BookHaven.Areas.Identity.Pages.Account
             public string ConfirmPassword { get; set; }
 
 
+            [Required]
+            public string? Name { get; set; }
+            public string? StreetAddress { get; set; }
+            public string? City { get; set; }
+            public string? State { get; set; }
+            public string? PostalCode { get; set; }
+            public string? PhoneNumber { get; set; }
+
             public string? Role { get; set; }
             [ValidateNever]
             public IEnumerable<SelectListItem> RoleList { get; set; }
@@ -143,6 +151,12 @@ namespace BookHaven.Areas.Identity.Pages.Account
 
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
+                user.Name = Input.Name;
+                user.StreetAddress = Input.StreetAddress;
+                user.City = Input.City;
+                user.State = Input.State;
+                user.PostalCode = Input.PostalCode;
+                user.PhoneNumber = Input.PhoneNumber;
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
                 if (result.Succeeded)
